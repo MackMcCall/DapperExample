fb5b43726208786b6c93bcc17e6e3e29771caaa5
Mack McCall <mackmc9924@gmail.com>
2023-08-18 10:09:53 -0600

    continued following exercise

---
+++ DapperExample/IDepartmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperExample
+{
+    public interface IDepartmentRepository
+    {
+        public IEnumerable<Department> GetAllDepartments(); 
+        //Stubbed out method for selecting departments
+    }
+}
--- DapperExample/Program.cs
+++ DapperExample/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using DapperExample;
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+
+var repo = new DepartmentRepository(conn);
+
+repo.InsertDepartment("Better Than Cool Stuff");
+
+var departments = repo.GetAllDepartments();
+
+foreach (var dep in departments)
+{
+    Console.WriteLine($"{dep.Name} | {dep.DepartmentID}");
+}
